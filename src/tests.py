881506c906cfd38a785246b134ceac792579559f
zakmoCA <zakeriyaamohamed@gmail.com>
2023-11-28 21:38:07 +1000

    tests passing with refactor

--- src/tests.py
+++ src/tests.py
@@ -1,18 +1,18 @@
 import unittest
 from unittest.mock import patch, mock_open
-from pomodoro_length import pomodoro_length
-from start_pomodoro import start
+from p_length_refactor import user_preferences
+from s_pomo_refactor import start
 
 
 class TestPomodoroApp(unittest.TestCase):
     # test that my pomodoro_length function correctly handles user input
     def test_pomodoro_length(self):
         with patch('builtins.input', side_effect=['4', '25', '5', '15']):
-            cycle_length, work_time, short_break_time, long_break_time = pomodoro_length()
+            cycle_length, work_time, short_break, long_break = user_preferences()
             self.assertEqual(cycle_length, 4) # checking if cycle_length is correct
             self.assertEqual(work_time, 25) # checking if work_time is correct
-            self.assertEqual(short_break_time, 5) # checking if short_break_time is correct
-            self.assertEqual(long_break_time, 15) # checking if long_break_time is correct
+            self.assertEqual(short_break, 5) # checking if short_break_time is correct
+            self.assertEqual(long_break, 15) # checking if long_break_time is correct
     """"
     My second test checks that my start funciton correctly writes something to csv.
     I can't check the exact timestamps are 'corect' because assert_called_once_with doesn't support regex
@@ -20,7 +20,7 @@ class TestPomodoroApp(unittest.TestCase):
       > the headers and pomodoro are written correctly
       > the string that the write method is called with starts with the task name, and has the structure we expect
     """
-    @patch('start_pomodoro.run_pomodoro')
+    @patch('s_pomo_refactor.run_pomodoro')
     @patch('builtins.input')
     @patch('builtins.open', new_callable=mock_open)
     @patch('os.path.isfile')
